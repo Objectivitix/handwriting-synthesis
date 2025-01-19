8d695a77d43adff78fa8cdd941fafff4c4ff3daf
Objectivitix <79152594+Objectivitix@users.noreply.github.com>
2025-01-19 12:43:04 -0500

    Allow longer prime sequences

--- demo.py
+++ demo.py
@@ -75,7 +75,7 @@ class Hand(object):
         max_tsteps = 40*max([len(i) for i in lines])
         biases = biases if biases is not None else [0.5]*num_samples
 
-        x_prime = np.zeros([num_samples, 1200, 3])
+        x_prime = np.zeros([num_samples, 1800, 3])
         x_prime_len = np.zeros([num_samples])
         chars = np.zeros([num_samples, self.max_line_length + 45])
         chars_len = np.zeros([num_samples])
